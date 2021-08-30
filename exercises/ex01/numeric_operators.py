5a4173fdaf162b4893d06db234d2be7f3afd06e0
Sarah <skateb@email.unc.edu>
2021-08-29 21:55:27 -0400

    updated snake case ex01

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,9 +2,9 @@
 
 __author__ = "730390226"
 
-x: int = int(input("Left-hand side: "))
-y: int = int(input("Right-hand side: "))
-print(str(x) + " ** " + str(y) + " is " + str(x ** y))
-print(str(x) + " / " + str(y) + " is " + str(x / y))
-print(str(x) + " // " + str(y) + " is " + str(x // y))
-print(str(x) + " % " + str(y) + " is " + str(x % y))
+left_hand: int = int(input("Left-hand side: "))
+right_hand: int = int(input("Right-hand side: "))
+print(str(left_hand) + " ** " + str(right_hand) + " is " + str(left_hand ** right_hand))
+print(str(left_hand) + " / " + str(right_hand) + " is " + str(left_hand / right_hand))
+print(str(left_hand) + " // " + str(right_hand) + " is " + str(left_hand // right_hand))
+print(str(left_hand) + " % " + str(right_hand) + " is " + str(left_hand % right_hand))
